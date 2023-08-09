3526babc5756392a972a1219fd12dd44d8194fb5
MandalSoham <141804174+MandalSoham@users.noreply.github.com>
2023-08-09 11:18:33 +0530

    Add files via upload

---
+++ methods.js
@@ -0,0 +1,40 @@
+function clickHome() {
+    var x = document.getElementById('Home1');
+    x.style.display = 'block';
+    x = document.getElementById('About1');
+    x.style.display = 'none';
+    x = document.getElementById('Portfolio1');
+    x.style.display = 'none';
+    x = document.getElementById('Contact1');
+    x.style.display = 'none';
+}
+function clickAbout() {
+    var x = document.getElementById('Home1');
+    x.style.display = 'none';
+    x = document.getElementById('About1');
+    x.style.display = 'block';
+    x = document.getElementById('Portfolio1');
+    x.style.display = 'none';
+    x = document.getElementById('Contact1');
+    x.style.display = 'none';
+}
+function clickPortfolio() {
+    var x = document.getElementById('Home1');
+    x.style.display = 'none';
+    x = document.getElementById('About1');
+    x.style.display = 'none';
+    x = document.getElementById('Portfolio1');
+    x.style.display = 'block';
+    x = document.getElementById('Contact1');
+    x.style.display = 'none';
+}
+function clickContact() {
+    var x = document.getElementById('Home1');
+    x.style.display = 'none';
+    x = document.getElementById('About1');
+    x.style.display = 'none';
+    x = document.getElementById('Portfolio1');
+    x.style.display = 'none';
+    x = document.getElementById('Contact1');
+    x.style.display = 'block';
+}
